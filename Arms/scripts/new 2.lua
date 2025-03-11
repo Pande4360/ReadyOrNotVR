64dfa67c73300e5f8545d5d8eedc0a1e009ee346
Pande4360 <114622967+Pande4360@users.noreply.github.com>
2025-03-11 16:17:02 +0100

    Added Arms Profile
    visible hands

---
+++ Arms/scripts/new 2.lua
@@ -0,0 +1,15 @@
+	local api = uevr.api
+	
+	local params = uevr.params
+	local callbacks = params.sdk.callbacks
+	local pawn = api:get_local_pawn(0)
+	local vr=uevr.params.vr
+	local lossy_offset=Vector3f.new(0,math.pi/2,0)
+
+uevr.sdk.callbacks.on_pre_engine_tick(function(engine, delta)
+	 pawn = api:get_local_pawn(0)
+    local CurrentPitch=pawn.Mesh1P.AnimScriptInstance.AnimGraphNode_PivotBone.Rotation.Roll
+	lossy_offset.x=-CurrentPitch*math.pi/180
+	UEVR_UObjectHook.get_or_add_motion_controller_state(pawn.Mesh1P):set_rotation_offset(lossy_offset)
+	
+end)
